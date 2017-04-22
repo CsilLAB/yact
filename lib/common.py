4fa3e4af4385da464b987bef06492a9c3b433561
CsilLAB <bessenyei.csilla@gmail.com>
2017-04-22 11:27:37 +0100

    Basic sturcture

---
+++ lib/common.py
@@ -0,0 +1,9 @@
+import json
+import os
+
+DIRECTORY = os.getcwd().replace('/lib','');
+
+def load_json_from_file(json_file):
+    """ Return json data """
+    with open(json_file) as json_data:
+        return = json.load(json_data)
---
+++ lib/output_text.py
@@ -0,0 +1,3 @@
+""" Display text """
+
+import common 
