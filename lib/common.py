0e088a7cad3d85bca512d102d2f5bd531f6cb9f0
CsilLAB <bessenyei.csilla@gmail.com>
2017-04-22 14:03:04 +0100

    text output

--- lib/common.py
+++ lib/common.py
@@ -1,9 +1,9 @@
 import json
 import os
 
-DIRECTORY = os.getcwd().replace('/lib','');
+DIRECTORY = os.getcwd().replace('lib','');
 
 def load_json_from_file(json_file):
     """ Return json data """
-    with open(json_file) as json_data:
-        return = json.load(json_data)
+    with open(DIRECTORY + json_file) as json_data:
+        return json.load(json_data)
